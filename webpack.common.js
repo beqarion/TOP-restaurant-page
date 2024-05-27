036eaa5c39a70d604bc0c135cfb06f2aeb96bf20
beqarion <beqarioni@yahoo.co.uk>
2024-05-27 14:16:36 +0400

    header fixed

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -43,6 +44,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/images/favicon.png",
     }),
   ],
   optimization: {
